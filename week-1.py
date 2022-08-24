2a1f622114a483c21dd6db93c5cc5a10b7c69750
ritukanta <reddyritukanta1925@gmail.com>
2022-08-24 19:23:21 +0530

    Quizzes..............

---
+++ week-1.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env pythn3
+
+# week 1
+# intro to Python
+# 1
+print("My first Python Program")
+
+# 3
+print("Have a nice day")
+
+# 4
+for i in range(5):
+    print("This is fun!")
+
+
+# hello word!
+# 4
+print("Programming in Python is fun!")
+
+# 5
+ratio = (1 + 5**(1/2))/2
+print(ratio)
+
+# moodule 1 graded assessment
+# 6
+print("Automating with Python is fun!")
+
+# 7
+color = "Yellow"
+thing = "sunshine"
+print(color + " is the color of " + thing)
+
+# 8
+seconds_per_day = 86400
+seconds_per_week = 7*seconds_per_day
+print(seconds_per_week)
+
+# 9
+# for 1 letter, there are 26 possibilities
+# so, for 6 letters
+print(26**6)
+
+
+# 10
+disk_size = 16*(1024**3)
+sector_size = 512
+sector_amount = disk_size / sector_size
+
+print(sector_amount)
---
+++ week-2.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+
+# expressions and variables
+# 1
+# bill comes in the amount of $47.28
+bill = 47.28
+# tip of 15%
+tip = bill * (15/100)
+total = bill + tip
+share = total/2
+print("Each person needs to pay: " + str(share))
+
+
+# 2
+numerator = 10
+denominator = 0
+result = numerator / (denominator+10)
+print(int(result))
+
+
+# 3
+word1 = "How"
+word2 = "do"
+word3 = "you"
+word4 = "like"
+word5 = "Python"
+word6 = "so"
+word7 = "far?"
+print(word1, word2, word3, word4, word5, word6, word7)
+
+
+# 4
+print("2 + 2 = " + str(2 +2 ))
+
+
+# functions
+# 1
